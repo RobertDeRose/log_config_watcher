bb9cf1773a061c3a67cb08ee877926c635ece26c
Robert DeRose <rderose@checkpt.com>
2023-01-09 13:33:17 -0500

    Make __version__ come from pyproject.toml file

--- log_config_watcher/__init__.py
+++ log_config_watcher/__init__.py
@@ -1,3 +1,8 @@
-__version__ = '0.1.0'
-
+import configparser
 from .log_config_watcher import LogConfigWatcher
+
+parser = configparser.ConfigParser()
+parser.read("pyproject.toml")
+__version__ = parser["tool.poetry"]["version"].strip('"')
+
+__all__ = ["__version__", "LogConfigWatcher"]
--- tests/test_log_confg_watcher.py
+++ tests/test_log_confg_watcher.py
@@ -1,5 +1,5 @@
-from log_confg_watcher import __version__
+from log_config_watcher import __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == '0.1.2'
